c0b65ab2be8312a287ffb51fec1eed271343c6c8
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-06-07 20:43:35 +0300

    Serializers and views added for User and Subscription models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,20 +1,31 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
-# from rest_framework import serializers
+from rest_framework import serializers
 from users.models import User
+
+STANDART_FIELDS = ('email', 'id', 'username', 'first_name', 'last_name',)
 
 
 class CustomUserSerializer(UserSerializer):
-    # is_subscribed = serializers.SerializerMethodField(read_only=True)
+    is_subscribed = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name')
-        # fields = ('email', 'id', 'username', 'first_name', 'last_name', 'is_subscribed')
+        fields = STANDART_FIELDS + ('is_subscribed',)
+
+    def get_is_subscribed(self, obj):
+        return obj.author.filter(
+            subscriber=self.context.get('request').user.id).exists()
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = User
-        fields = (
-            'email', 'id',  'username', 'first_name', 'last_name', 'password'
-        )
+        fields = STANDART_FIELDS + ('password',)
+
+
+class SubscriptionSerializer(CustomUserSerializer):
+    class Meta(CustomUserSerializer.Meta):
+        # fields = ('id', 'email', 'username', 'first_name',
+        #           'last_name', 'is_subscribed', 'recipes', "recipes", 'recipes_count')
+        fields = STANDART_FIELDS + ('is_subscribed',)
+        read_only_fields = STANDART_FIELDS + ('is_subscribed',)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,47 @@
 from api.pagination import StandardResultsSetPagination
+from api.serializers import SubscriptionSerializer
+from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from users.models import Subscription, User
 
 
 class CustomUserViewSet(UserViewSet):
     pagination_class = StandardResultsSetPagination
+
+    @action(methods=['DELETE', 'POST'], detail=True,
+            permission_classes=[IsAuthenticated])
+    def subscribe(self, request, id):
+        subscriber = request.user
+        author = get_object_or_404(User, id=id)
+        subscription = Subscription.objects.filter(subscriber=subscriber,
+                                                   author=author)
+        if request.method == 'DELETE':
+            if subscription.exists():
+                subscription.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response((f'Вы не подписаны на {author.username}'),
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if subscription.exists():
+            return Response((f'Вы уже подписаны на {author.username}'),
+                            status=status.HTTP_400_BAD_REQUEST)
+        if author == subscriber:
+            return Response('Невозможно подписаться на себя',
+                            status=status.HTTP_400_BAD_REQUEST)
+        serializer = SubscriptionSerializer(author, data=request.data,
+                                            context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        Subscription.objects.create(subscriber=subscriber, author=author)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, permission_classes=[IsAuthenticated])
+    def subscriptions(self, request):
+        queryset = User.objects.filter(author__subscriber=request.user)
+        page = self.paginate_queryset(queryset)
+        serializer = SubscriptionSerializer(page, context={'request': request},
+                                            many=True)
+        return self.get_paginated_response(serializer.data)
