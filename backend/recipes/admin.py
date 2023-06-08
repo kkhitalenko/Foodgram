ed32ba36a7ad792c0dbd9a875b5ffaf24fbfb00e
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-06-08 21:25:53 +0300

    Ingredient, Tag, Recipe, IngredientInRecipe models added

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from recipes.models import Ingredient, IngredientInRecipe, Recipe, Tag
 
-# Register your models here.
+
+class IngredientInRecipeInline(admin.TabularInline):
+    model = IngredientInRecipe
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'slug')
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ingredient', 'recipe', 'quantity')
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name')
+    # list_display = ('id', 'author', 'name', 'added_to_favorite')
+    list_filter = ('author', 'name', 'tags')
+
+    inlines = (IngredientInRecipeInline,)
+
+    # @admin.display(description='Добавлено в избранное')
+    # def added_to_favorite(self, obj):
+    #     return ...
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,88 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """ORM model for ingredient."""
+
+    name = models.CharField(max_length=200, verbose_name='Ингридиент')
+    quantity = models.IntegerField(validators=[MinValueValidator(1)],
+                                   verbose_name='Количество')
+    measurement_unit = models.CharField(max_length=200,
+                                        verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        constraints = [models.UniqueConstraint(
+            fields=('name', 'measurement_unit'),
+            name='unique_ingredient_measurement_unit')]
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """ORM model for tag."""
+
+    name = models.CharField(max_length=200, unique=True, verbose_name='Тег')
+    color = models.CharField(max_length=7, unique=True, verbose_name='HEX-код')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Адрес')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    """ORM model for recipe."""
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='recipe', verbose_name='Автор')
+    name = models.CharField(max_length=200, verbose_name='Рецепт')
+    image = models.ImageField(upload_to='images/', verbose_name='Картинка')
+    text = models.TextField(verbose_name='Описание')
+    ingredients = models.ManyToManyField(Ingredient,
+                                         through='IngredientInRecipe',
+                                         related_name='recipe',
+                                         verbose_name='Ингредиенты')
+    tags = models.ManyToManyField(Tag, related_name='recipe',
+                                  verbose_name='Теги')
+    cooking_time = models.IntegerField(validators=[MinValueValidator(1)],
+                                       verbose_name='Время в минутах')
+    pub_date = models.DateField(auto_now_add=True,
+                                verbose_name='Дата публикации')
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientInRecipe(models.Model):
+    """ORM model for ingredients quantity in recipe."""
+
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   related_name='ingredient_in_recipe',
+                                   verbose_name='Ингредиент')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+                               related_name='ingredient_in_recipe',
+                               verbose_name='Рецепт')
+    quantity = models.IntegerField(validators=[MinValueValidator(1)],
+                                   verbose_name='Количество')
+
+    class Meta:
+        verbose_name = verbose_name_plural = 'Количество ингридиента в рецепте'
+        constraints = [models.UniqueConstraint(
+                fields=('ingredient', 'recipe'),
+                name='unique_ingredient_recipe')]
+
+    def __str__(self):
+        return f'{self.quantity}'
