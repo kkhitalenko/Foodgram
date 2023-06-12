8e864f4846f5358505011e59ad25f1399d99bb7c
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-06-12 17:47:32 +0300

    Updated with an usage of decorator

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from users.models import Subscription, User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'username', 'first_name', 'last_name')
     list_filter = ('email', 'username')
 
 
+@admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('subscriber', 'author')
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Subscription, SubscriptionAdmin)
