6d24637160c9d192d705500355dc1f479b099caf
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-06-19 15:44:17 +0300

    Import ingredients from ingredients.json command added

---
+++ backend/api/management/commands/import_json.py
@@ -0,0 +1,27 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from api.serializers import IngredientSerializer
+
+filepath = '../data/ingredients.json'
+
+
+class Command(BaseCommand):
+    help = "Import ingredients from ingredients.json."
+
+    def handle(self, *args, **options):
+        try:
+            with open(filepath, encoding='utf-8') as json_file:
+                ingredients = json.load(json_file)
+                for ingredient in ingredients:
+                    serializer = IngredientSerializer(data=ingredient)
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Ошибка в файле {serializer.errors}'
+                        ))
+                self.stdout.write(self.style.SUCCESS('Файл успешно загружен'))
+        except Exception as e:
+            raise Exception(f'Ошибка: {e}')
