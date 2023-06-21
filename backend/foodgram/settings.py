f51549e5443154826db317300e06a6f3366ec1ba
Екатерина Хиталенко <ekaterinakhitalenko@gmail.com>
2023-06-21 16:33:49 +0300

    Added default engine in databases

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,8 +1,7 @@
 import os
-
-from dotenv import load_dotenv
 from pathlib import Path
 
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -82,7 +81,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
